1ca7b28962282d934f5afadf9f43e093ffb6556e
Nikola Jevric <46942531+NJevric@users.noreply.github.com>
2020-02-26 23:42:42 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -261,30 +261,30 @@ function filterKategorija(){
         }
     })
 }
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
   
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 1; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
+//   if (n > slides.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 1; i < slides.length; i++) {
+//       slides[i].style.display = "none";  
+//   }
  
-  slides[slideIndex-1].style.display = "block";  
+//   slides[slideIndex-1].style.display = "block";  
   
-}
+// }
 }
 
 //WOMAN STRANA
